8cbcc220edbda6d034d05c7f200e64d954bc022e
= <2021ugcs095@nitjsr.ac.in>
2023-08-04 23:16:22 +0530

    areingo

--- backend/index.js
+++ backend/index.js
@@ -13,14 +13,14 @@ global.foodData = require('./db')(function call(err, data, CatData) {
   const cors=require('cors');
   app.use(cors());
   const port = process.env.PORT;
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  //   res.header(
+  //     "Access-Control-Allow-Headers",
+  //     "Origin, X-Requested-With, Content-Type, Accept"
+  //   );
+  //   next();
+  // });
   app.use(express.json())
   
   app.get('/', (req, res) => {
